5e62509acf7ff100a146a4c9216f28f85e508ef8
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-11 19:37:31 -0400

    javaandroid show edittext content on click

--- cli-build/86/projects/java-android/java/com/crazyj36/javaandroid/MainActivity.java
+++ cli-build/86/projects/java-android/java/com/crazyj36/javaandroid/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.Button;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.text.*;
 
 public class MainActivity extends Activity {
   @Override
@@ -48,32 +47,20 @@ public class MainActivity extends Activity {
       button2.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-          textView1.setText("hi");
+          textView1.setText("TextView Shown");
 
         }
       });
 
       final EditText editText = findViewById(R.id.editText);
       final TextView textView2 = findViewById(R.id.textView2);
-      editText.addTextChangedListener(new TextWatcher() {
-        @Override
-        public void afterTextChangedListener(Editable s) {
-        }
-        @Override
-        public void beforTextChangedListener(CharSequence s, int start, int count, int after) {
-        }
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-          textView2.setText(editText.getText());
-        }
-      });
-/*      Button button3 = findViewById(R.id.button3);
+      Button button3 = findViewById(R.id.button3);
       button3.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
               textView2.setText(editText.getText());
           }
-      });*/
+      });
 
     }
 }
