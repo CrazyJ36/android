09f0921fe03f24ea971280e4a4afd808314b6f30
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-19 00:13:22 -0400

    cli luncher u

--- cli-build/86/projects/launcher/build/gen/com/crazyj36/launcher/R.java
+++ cli-build/86/projects/launcher/build/gen/com/crazyj36/launcher/R.java
@@ -11,11 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int btn=0x7f030001;
         public static final int lv=0x7f030000;
-        public static final int tv=0x7f030002;
     }
     public static final class layout {
         public static final int activity_main=0x7f020000;
+        public static final int list_layout=0x7f020001;
     }
 }
