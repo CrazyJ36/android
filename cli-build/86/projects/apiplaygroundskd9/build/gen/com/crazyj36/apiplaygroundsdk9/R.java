e178e8025b45cac5b1fa7fe431805eea58df5029
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-07 04:20:42 -0400

    apiplaygroundsdk9: revert CODENAME text feild

--- cli-build/86/projects/apiplaygroundskd9/build/gen/com/crazyj36/apiplaygroundsdk9/R.java
+++ cli-build/86/projects/apiplaygroundskd9/build/gen/com/crazyj36/apiplaygroundsdk9/R.java
@@ -24,16 +24,15 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f030000;
-        public static final int btn1Txt=0x7f030002;
-        public static final int dialogBtn1Txt=0x7f030005;
-        public static final int dialogBtn2Txt=0x7f030006;
-        public static final int dialogTitle=0x7f030007;
-        /** Dialog
-         */
-        public static final int dialogTv1Txt=0x7f030003;
-        public static final int dialogTv2Txt=0x7f030004;
         /** Activity Main
          */
-        public static final int tv1Txt=0x7f030001;
+        public static final int btn1Txt=0x7f030001;
+        public static final int dialogBtn1Txt=0x7f030004;
+        public static final int dialogBtn2Txt=0x7f030005;
+        public static final int dialogTitle=0x7f030006;
+        /** Dialog
+         */
+        public static final int dialogTv1Txt=0x7f030002;
+        public static final int dialogTv2Txt=0x7f030003;
     }
 }
