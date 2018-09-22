0e53d3aec76c82789257b6cc2f97791fe16c647f
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-22 02:03:20 -0400

    organize

---
+++ cli-build/86/projects/apiplaygroundsdk9/java/com/crazyj36/apiplaygroundsdk9/MainActivity.java
@@ -0,0 +1,91 @@
+package com.crazyj36.apiplaygroundsdk9;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Device SDK Version view
+        TextView tv1 = findViewById(R.id.tv1);
+        tv1.setText(String.format(Locale.US, "%s %d", getString(R.string.tv1Txt), android.os.Build.VERSION.SDK_INT));
+
+        // Dialog with layout example
+        Button button = findViewById(R.id.btn1);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.setContentView(R.layout.dialog_layout);
+                dialog.setTitle("A Dialog");
+                dialog.show();
+                dialog.findViewById(R.id.dialogBtn1).setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "Dialog Button Pushed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.findViewById(R.id.dialogBtn2).setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        // Concatenate two strings, c-like
+        final TextView tvSet = findViewById(R.id.tvSet);
+        String txt1 = "Hello";
+        String txt2 = "World";
+        tvSet.setText(String.format(Locale.US, "%s %s!", txt1, txt2));
+
+        // Logs
+        String startMsg = "App Started";
+        Log.i("JAVAANDROIDLOG", startMsg);
+        Button btnLog = findViewById(R.id.btnLog);
+        btnLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("JAVAANDROIDLOG", "Log Button clicked");
+            }
+        });
+
+        // Java package Date()
+        final TextView tvDate = findViewById(R.id.tvDate);
+        final String date = new Date().toString();
+        Button btnDate = findViewById(R.id.btnDate);
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDate.setText(date);
+            }
+        });
+
+        // set textview to what was entered in editText
+        final EditText etPrint = findViewById(R.id.etPrint);
+        final TextView tvPrint = findViewById(R.id.tvPrint);
+        Button btnPrint = findViewById(R.id.btnPrint);
+        btnPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvPrint.setText(etPrint.getText());
+            }
+        });
+
+    }
+}
