fad0bc9696bd4a321770bf491ad46260a1edb40f
CrazyJ36 <crazy_j36@hotmail.com>
2018-10-16 21:45:29 -0400

    cli-build/template: update AndroidManifest.xml to use device default sdk theme

--- cli-build/86/projects/template/java/com/crazyj36/template/MainActivity.java
+++ cli-build/86/projects/template/java/com/crazyj36/template/MainActivity.java
@@ -8,9 +8,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-   	setContentView(R.layout.activity_main);
+
+	   	setContentView(R.layout.activity_main);
 
         TextView text = (TextView) findViewById(R.id.textView);
         text.setText("Template App");
+
     }
 }
