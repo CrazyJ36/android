a95a98d742fd31eb433410e0bce8b1e4b6607bb6
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-02 13:18:55 -0400

    apiplayground: widget edit

--- projects/apiplayground/app/src/main/java/com/crazyj36/apiplayground/AppWidget.java
+++ projects/apiplayground/app/src/main/java/com/crazyj36/apiplayground/AppWidget.java
@@ -5,7 +5,11 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+
 public class AppWidget extends AppWidgetProvider {
+
+
+
     public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
         final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget);
         remoteViews.setTextViewText(R.id.tvWidget, context.getResources().getText(R.string.bigText));
