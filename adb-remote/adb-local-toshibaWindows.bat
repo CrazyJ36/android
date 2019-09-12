35f47f9935a3f0d4f98a6d9d7738c143682619f5
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-11 20:24:23 -0400

    adb-remote: change adb-local-toshibaWindows ip

--- adb-remote/adb-local-toshibaWindows.bat
+++ adb-remote/adb-local-toshibaWindows.bat
@@ -1,1 +1,1 @@
-java -jar C:\Users\Thomas\Development\android\adb-remote\adbportforward.jar server adblocation=C:\Users\Thomas\development\android\adb-remote\platform-tools
+java -jar C:\Users\Thomas\development\android\adb-remote\adbportforward.jar server adblocation=C:\Users\Thomas\development\android\adb-remote\platform-tools
