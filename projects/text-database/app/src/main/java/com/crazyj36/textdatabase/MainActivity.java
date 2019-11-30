b34c6ed69f02480c3dd51b8de912d5a4bf64db26
CrazyJ36 <crazy_j36@hotmail.com>
2019-11-30 18:07:39 -0500

    finish text database

--- projects/text-database/app/src/main/java/com/crazyj36/textdatabase/MainActivity.java
+++ projects/text-database/app/src/main/java/com/crazyj36/textdatabase/MainActivity.java
@@ -1,8 +1,10 @@
 package com.crazyj36.textdatabase;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
@@ -15,28 +17,40 @@ import java.io.IOException;
 
 
 public class MainActivity extends Activity {
-
+FileWriter fileWriter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) &&
+                    !(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED)) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
         final EditText editText1 = findViewById(R.id.editText1);
-        String file_dir = Environment.getExternalStorageDirectory()
-                .toString() + "/text_database.txt";
-        final File data_file = new File(file_dir);
-        Toast.makeText(this, data_file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-        
         Button button1 = findViewById(R.id.buttton1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String file_dir = Environment.getExternalStorageDirectory()
+                        .toString() + "/text_database.txt";
+                File data_file = new File(file_dir);
                 try {
                     FileWriter fileWriter = new FileWriter(data_file, true);
-                    fileWriter.append(editText1.getText());
+                    String input = editText1.getText() + "\n";
+                    fileWriter.append(input);
                     fileWriter.close();
                 } catch (IOException ioException) {
-                    Toast.makeText(MainActivity.this, "Io Exception" + ioException.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,
+                            "Could initialize FileWriter for database file.\n" +
+                                    ioException.getLocalizedMessage(),
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
