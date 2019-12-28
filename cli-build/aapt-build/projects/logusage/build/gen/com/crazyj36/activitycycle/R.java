b476a6837350468da95dcc6c5038ab30a36d574b
CrazyJ36 <crazy_j36@hotmail.com>
2019-12-28 02:31:52 -0500

    u

--- cli-build/aapt-build/projects/logusage/build/gen/com/crazyj36/activitycycle/R.java
+++ cli-build/aapt-build/projects/logusage/build/gen/com/crazyj36/activitycycle/R.java
@@ -5,18 +5,18 @@
  * should not be modified by hand.
  */
 
-package com.crazyj36.template;
+package com.crazyj36.activitycycle;
 
 public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int textView=0x7f040000;
+        public static final int tvStatus=0x7f040000;
     }
     public static final class layout {
         public static final int activity_main=0x7f020000;
     }
     public static final class string {
-        public static final int appname=0x7f030000;
+        public static final int tvStatusStartTxt=0x7f030000;
     }
 }
