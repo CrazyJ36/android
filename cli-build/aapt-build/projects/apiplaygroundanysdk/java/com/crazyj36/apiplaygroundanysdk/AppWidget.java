1283658df814f5bd482c441a8147e0031afa51be
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-20 13:25:44 -0500

    apiplayroundanysdk: better widget

--- cli-build/aapt-build/projects/apiplaygroundanysdk/java/com/crazyj36/apiplaygroundanysdk/AppWidget.java
+++ cli-build/aapt-build/projects/apiplaygroundanysdk/java/com/crazyj36/apiplaygroundanysdk/AppWidget.java
@@ -7,16 +7,24 @@ import android.widget.RemoteViews;
 import android.view.View;
 import android.os.Build.VERSION;
 import android.widget.Toast;
+import android.content.Intent;
+import android.app.PendingIntent;
 
 public class AppWidget extends AppWidgetProvider {
 
 	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
-		if (VERSION.SDK_INT >= 3) {
-            final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget);
-			remoteViews.setTextViewText(R.id.tvWidget, "widget content text");
-	        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
-		} else {
-			Toast.makeText(context.getApplicationContext(), "AppWidgetProvider not available in API 3. I should workaround it if I ever get A device that old", Toast.LENGTH_SHORT).show();
-		}
+            if (VERSION.SDK_INT >= 3) {
+                final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget);
+	            remoteViews.setTextViewText(R.id.tvWidget, "widget content text");
+
+                Intent startMainIntent = new Intent(context, MainActivity.class);
+                PendingIntent pendingStartMain = PendingIntent.getActivity(context, 0, startMainIntent, 0);
+                remoteViews.setOnClickPendingIntent(R.id.widgetBtn, pendingStartMain);
+
+                appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+	    } else {
+		Toast.makeText(context.getApplicationContext(), "AppWidgetProvider not available in API 3.", Toast.LENGTH_SHORT).show();
+	    }
+
 	}
 }
