4afe9fbb819d670ef7a530123e66065eb104a60d
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-23 17:26:25 -0500

    cli-build: finish newactivity

--- cli-build/aapt-build/projects/newactivity/java/com/crazyj36/newactivity/MainActivity.java
+++ cli-build/aapt-build/projects/newactivity/java/com/crazyj36/newactivity/MainActivity.java
@@ -2,15 +2,18 @@ package com.crazyj36.newactivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 	   	setContentView(R.layout.activity_main);
+    }
 
-
-
+    public void newActivity(View v) {
+        Intent nextActivity = new Intent(MainActivity.this, NextActivity.class);
+        startActivity(nextActivity);
     }
 }
