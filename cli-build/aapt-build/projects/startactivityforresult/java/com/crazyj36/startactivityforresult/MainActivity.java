e01e00494b58c0670785d0ad69fb6e50b559580d
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-25 01:56:50 -0500

    u

--- cli-build/aapt-build/projects/startactivityforresult/java/com/crazyj36/startactivityforresult/MainActivity.java
+++ cli-build/aapt-build/projects/startactivityforresult/java/com/crazyj36/startactivityforresult/MainActivity.java
@@ -8,7 +8,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    // Makeshift reference to match amongst intents later.
     int INTENT_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +21,8 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                /* For intents here:
-                   ACTION_PICK_ACTIVITY: returns working intent(data) to chosen activity.
-                     and data.toUri(0) will show to full intent string.
-                     Can startActivity(data) in onActivityResult()
-                */
+                /* ACTION_PICK_ACTIVITY: As started intent, returns working intent(data) string that can start picked activity.
+                using onActivityResult() Intent data */
                 Intent chooseIntent = new Intent(Intent.ACTION_PICK_ACTIVITY);
                 startActivityForResult(chooseIntent, INTENT_REQUEST);
             }
@@ -32,17 +32,26 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
        super.onActivityResult(requestCode, resultCode, data);
+
+       /* Use same INTENT_REQUEST code as starting  intent */
        if (requestCode == INTENT_REQUEST && resultCode == RESULT_OK) {
-           TextView tvResult = findViewById(R.id.tvResult);
-           String dataResult = data.toUri(0);
-           tvResult.setText(dataResult);
+
 	       Button btnLaunch = findViewById(R.id.btnLaunch);
 	       btnLaunch.setOnClickListener(new View.OnClickListener() {
 	           @Override
 	           public void onClick(View view) {
+
+                   /* Here, intent data from startActivityResult() is used with the same intent string */
 	               startActivity(data);
+
 	           }
 	       });
+
+           /* Get the string that was recieved during onActivityResult() */
+           TextView tvResult = findViewById(R.id.tvResult);
+           String dataResult = data.toUri(0);
+           tvResult.setText(dataResult);
+
        }
    }
 }
