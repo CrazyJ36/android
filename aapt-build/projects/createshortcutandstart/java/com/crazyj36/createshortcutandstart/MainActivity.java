e5b80d6062375d4fde6d3d1758a9622907e203ec
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-28 00:16:10 -0500

    u

--- aapt-build/projects/createshortcutandstart/java/com/crazyj36/createshortcutandstart/MainActivity.java
+++ aapt-build/projects/createshortcutandstart/java/com/crazyj36/createshortcutandstart/MainActivity.java
@@ -15,18 +15,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 	   	setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.btnChoose);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        ((Button)findViewById(R.id.btnChoose)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                /* ACTION_CREATE_SHORTCUT: Used with Intent, returns parcelable intent(data) representing the shortcut
-                use startActivityForResult and onActivityResult() to chosen shortcut. */
-                Intent chooseIntent = new Intent(Intent.ACTION_CREATE_SHORTCUT);
+                /* ACTION_CREATE_SHORTCUT: Used with Intent, returns parcelable intent(data) representing the shortcut.
+                Use startActivityForResult and onActivityResult() to use or get info from chosen shortcut. */
+                Intent chooseIntent = new Intent(Intent.ACTION_CREATE_SHORTCUT); // or string "android.intent.action.CREATE_SHORTCUT"
                 startActivityForResult(chooseIntent, INTENT_REQUEST);
             }
         });
+
     }
 
     @Override
@@ -36,24 +37,31 @@ public class MainActivity extends Activity {
        // Check intent result
        if (requestCode == INTENT_REQUEST && resultCode == RESULT_OK) {
 
-           // Show chosen shortcuts' intent and name.
-           TextView tvResult = findViewById(R.id.tvResult);
-           String dataResult = data.getStringExtra(Intent.EXTRA_SHORTCUT_NAME) //
-               + "\n" + data.toUri(0);
-           tvResult.setText(dataResult);
+           // get all intent bundle info fields into one string.
+           String info = new String();
+           for (String key : data.getExtras().keySet()) { // Bundle bundleOfExtras = data.getExtras() for array.
+               info = info + "\n" + key + "=\n" + data.getExtras().get(key) + "\n";
+           }
+           ((TextView)findViewById(R.id.tvResult)).setText(info); // Shortened cast
 
 	       Button btnLaunch = findViewById(R.id.btnLaunch);
 	       btnLaunch.setOnClickListener(new View.OnClickListener() {
 	           @Override
 	           public void onClick(View view) {
 
-                       // data can be parsed and the extra for received for the shortcuts' intent is android.intent.extra.shortcut.INTENT
+                       // get intent returned by CREATE_SHORTCUT. This is the only way I've come across.
+                       // getParcelableExtra(String) retrieves extended data from the intent, android.intent.extra.shortcut.INTENT being A defualt extra.
+                      //  in english: get the shortcuts' intent, which is A parcelable, from the intents' bundle.
+                       // simply doing startActivity(data) gets ActivityNotFoundException, so we have to make A new intent with previous intents extra.
                        Intent shortcutIntent = data.getParcelableExtra("android.intent.extra.shortcut.INTENT");
+                       // this works to, though deprecated.
+                       // Intent shortcutIntent = data.getParcelableExtra(Intent.EXTRA_SHORTCUT_INTENT);
                        startActivity(shortcutIntent);
 
 	           }
 	       });
 
        }
+
    }
 }
