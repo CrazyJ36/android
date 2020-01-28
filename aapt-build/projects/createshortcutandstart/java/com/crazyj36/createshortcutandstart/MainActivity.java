cca467aedd3c03ef16c0c214e0cc4b71b3ad9ca7
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-27 20:22:08 -0500

    finish choose and launch shortcut

--- aapt-build/projects/createshortcutandstart/java/com/crazyj36/createshortcutandstart/MainActivity.java
+++ aapt-build/projects/createshortcutandstart/java/com/crazyj36/createshortcutandstart/MainActivity.java
@@ -6,9 +6,6 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.TextView;
-import java.net.URISyntaxException;
-import android.content.pm.ShortcutInfo;
-import android.content.pm.ShortcutManager;
 
 public class MainActivity extends Activity {
 
@@ -24,10 +21,9 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                /* ACTION_CREATE_SHORTCUT: As started intent, returns intent(data) representing the ShortcutInfo() result.
-                using onActivityResult() Intent data */
+                /* ACTION_CREATE_SHORTCUT: Used with Intent, returns parcelable intent(data) representing the shortcut
+                use startActivityForResult and onActivityResult() to chosen shortcut. */
                 Intent chooseIntent = new Intent(Intent.ACTION_CREATE_SHORTCUT);
-                //chooseIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, null);
                 startActivityForResult(chooseIntent, INTENT_REQUEST);
             }
         });
@@ -37,29 +33,26 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
        super.onActivityResult(requestCode, resultCode, data);
 
-       /* Use same INTENT_REQUEST code as starting intent */
+       // Check intent result
        if (requestCode == INTENT_REQUEST && resultCode == RESULT_OK) {
+
+           // Show chosen shortcuts' intent and name.
+           TextView tvResult = findViewById(R.id.tvResult);
+           String dataResult = data.getStringExtra(Intent.EXTRA_SHORTCUT_NAME) //
+               + "\n" + data.toUri(0);
+           tvResult.setText(dataResult);
 
 	       Button btnLaunch = findViewById(R.id.btnLaunch);
 	       btnLaunch.setOnClickListener(new View.OnClickListener() {
 	           @Override
 	           public void onClick(View view) {
 
-                       ShortcutInfo.Builder shortcut = new ShortcutInfo.Builder(MainActivity.this, "id1");
-                       shortcut.setIntent(data);
-                       ShortcutInfo shortcutInfo = shortcut.build();
-                       ShortcutManager shortcutManager = createShortcutResultIntent(shortcutInfo);
+                       // data can be parsed and the extra for received for the shortcuts' intent is android.intent.extra.shortcut.INTENT
+                       Intent shortcutIntent = data.getParcelableExtra("android.intent.extra.shortcut.INTENT");
                        startActivity(shortcutIntent);
+
 	           }
 	       });
-
-           TextView tvResult = findViewById(R.id.tvResult);
-
-           // Gets Name of chosen shortcut
-           // String dataResult = data.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
-
-           String dataResult = data.toUri(Intent.URI_INTENT_SCHEME);
-           tvResult.setText(dataResult);
 
        }
    }
