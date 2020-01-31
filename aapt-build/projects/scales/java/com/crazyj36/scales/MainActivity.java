7247aa54b68128dfade256c712f90fcabed96c60
CrazyJ36 <crazy_j36@hotmail.com>
2020-01-30 19:39:35 -0500

    aapt-build: work on scales

--- aapt-build/projects/scales/java/com/crazyj36/scales/MainActivity.java
+++ aapt-build/projects/scales/java/com/crazyj36/scales/MainActivity.java
@@ -4,53 +4,50 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.CharArrayWriter;
 
-public class MainActivity Activity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // make inverted orders here starting with root notes, feed order to setmessage
+        final String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+        String[] scales = new String[]{"Major", "Minor"};
+
         final ListView listView;
         listView = (ListView) findViewById(R.id.list);
-
-        String orig = "C C#";
-        char note1 = orig.charAt(0);
-        String note2 = orig.substring(1,4);
-
-        final String var1 = ( note2+" " + note1);
-        Toast.makeText(getApplicationContext(), var1, Toast.LENGTH_LONG)
-                .show();
-
-        String[] values = new String[]{"Major", "Natural Minor", "Harmonic Minor", "Melodic Minor",
-                "Whole Tone", "Mixolydian", "Phrygian  Dominant", "Pentatonic Major",
-                "Pentatonic Minor",
-        };
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_expandable_list_item_1, android.R.id.text1, values);
-
+            android.R.layout.simple_expandable_list_item_1, android.R.id.text1, scales);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).create();
 
-                String itemValue = (String) listView.getItemAtPosition(position);
-                AlertDialog ipopUp = new AlertDialog.Builder(MainActivity.this).create();
-                ipopUp.setMessage("Variants of "+itemValue + "are:"+ var1);
-                ipopUp.setButton(AlertDialog.BUTTON_NEUTRAL, "Done", new DialogInterface.OnClickListener() {
+
+                String chosen = (String) listView.getItemAtPosition(position);
+                if (chosen == "Major") {
+                    // put these into a function with auto number/space.
+                    dialog.setMessage(notes[0] + " " + notes[2] + " " + notes[4] + " " + notes[5] + " " + notes[7] + " " + notes[9] + " " + notes[11] + " " + notes[0]);
+                } else if (chosen == "Minor") {
+                    dialog.setMessage(notes[0] + " " + notes[2] +  " " + notes[3] + " " + notes[5] + " " + notes[7] + " " + notes[8] + " " + notes[10] + " " + notes[0]);
+                }
+
+                dialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Done", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.dismiss();
                     }
                 });
-                ipopUp.show();
+                dialog.show();
             }
 
         });
