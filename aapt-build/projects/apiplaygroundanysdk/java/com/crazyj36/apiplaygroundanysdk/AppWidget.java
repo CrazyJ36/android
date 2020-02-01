b7615884fe789264a9731015a02f9cef782e954d
CrazyJ36 <crazy_j36@hotmail.com>
2020-02-01 17:47:06 -0500

    layout practice

--- aapt-build/projects/apiplaygroundanysdk/java/com/crazyj36/apiplaygroundanysdk/AppWidget.java
+++ aapt-build/projects/apiplaygroundanysdk/java/com/crazyj36/apiplaygroundanysdk/AppWidget.java
@@ -12,23 +12,43 @@ import android.app.PendingIntent;
 
 public class AppWidget extends AppWidgetProvider {
 
-    int updater = 0;
+    static int updater = 0;
 
-	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
-        if (VERSION.SDK_INT >= 3) {
-            final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget);
-
-            remoteViews.setTextViewText(R.id.tvWidget, String.valueOf(updater + 1));
+    // Overriden method defining the app widget update procudure.
+    static void myUpdateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    if (VERSION.SDK_INT >= 3) {
+            updater = updater + 1;
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.appwidget);
+            remoteViews.setTextViewText(R.id.tvWidget, String.valueOf(updater));
 
             Intent startMainIntent = new Intent(context, MainActivity.class);
             PendingIntent pendingStartMain = PendingIntent.getActivity(context, 0, startMainIntent, 0);
-            remoteViews.setOnClickPendingIntent(R.id.widgetBtn, pendingStartMain);
+            remoteViews.setOnClickPendingIntent(R.id.widgetLaunchBtn, pendingStartMain);
 
-            appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+            Intent intentUpdate = new Intent(context, AppWidget.class);;
+            intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
+            int[] idArray = new int[] {appWidgetId};
+            intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
+
+            PendingIntent pendingUpdate = PendingIntent.getBroadcast(context, appWidgetId, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.widgetUpdateBtn, pendingUpdate);
+            Toast.makeText(context, "myUpdateAppWidget() called.", 0).show();
+
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
 
 	    } else {
-		    Toast.makeText(context.getApplicationContext(), "AppWidgetProvider not available below Android API level 3.", Toast.LENGTH_SHORT).show();
+		    Toast.makeText(context, "AppWidgetProvider not available below Android API level 3.", Toast.LENGTH_SHORT).show();
 	    }
+    }
 
+    // Called when widget updates
+    @Override
+	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            myUpdateAppWidget(context, appWidgetManager, appWidgetId);
+            Toast.makeText(context, "onUpdate() widget called.", 0).show();
+        }
 	}
+
 }
