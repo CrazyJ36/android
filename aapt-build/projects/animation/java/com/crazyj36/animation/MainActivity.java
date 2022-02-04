5dec2f3fc652c11ed35b82f642c462376feae3bd
CrazyJ36 <crazy_j36@hotmail.com>
2022-02-03 23:18:43 -0500

    animate: programatic animation based on screen size

--- aapt-build/projects/animation/java/com/crazyj36/animation/MainActivity.java
+++ aapt-build/projects/animation/java/com/crazyj36/animation/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.ImageView;
 import android.widget.Button;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.util.DisplayMetrics;
+import android.widget.Toast;
 //import android.animation.ObjectAnimator;
 
 public class MainActivity extends Activity {
@@ -18,10 +21,10 @@ public class MainActivity extends Activity {
 	   	setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
-        ImageView imageView = findViewById(R.id.imageView);
+        final ImageView imageView = findViewById(R.id.imageView);
         // Load all animations and start them programatically below.
         Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
-        Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move2);
+        //Animation animation2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move2);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +44,20 @@ public class MainActivity extends Activity {
             }
             @Override
             public void onAnimationEnd(Animation animation1) {
-                imageView.startAnimation(animation2);
+                // Load A different animation xml file.
+                //imageView.startAnimation(animation2);
+
+               // Animation using code.
+               DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+               float parentWidth = displayMetrics.widthPixels;
+               Toast.makeText(MainActivity.this, "Screen width= " + String.valueOf(parentWidth), Toast.LENGTH_SHORT).show();
+
+               // fromX: if width is 1080p: start at zero(middel), minus half of width(gives right end).
+               // toX: to half percent of width as middle would be zero instead of 1080.
+               Animation translate = new TranslateAnimation(0 - (parentWidth / 2), parentWidth / 2, 0, 0);
+               translate.setDuration(800); // won't move without A duration.
+               translate.setFillAfter(true);
+               imageView.startAnimation(translate);
             }
             @Override
             public void onAnimationRepeat(Animation animation1) {
