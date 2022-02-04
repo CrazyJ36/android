9f0d52eb6d91b9f9265bb6ce9d2d5d42853989eb
CrazyJ36 <crazy_j36@hotmail.com>
2022-02-03 20:25:52 -0500

    finish animation example

--- aapt-build/projects/animation/java/com/crazyj36/animation/MainActivity.java
+++ aapt-build/projects/animation/java/com/crazyj36/animation/MainActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Context;
 import android.view.View;
-//import android.view.animation.Animation;
-//import android.view.animation.AnimationUtils;
-//import android.widget.ImageView;
+import android.widget.ImageView;
+import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 //import android.animation.ObjectAnimator;
 
 public class MainActivity extends Activity {
@@ -16,15 +17,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 	   	setContentView(R.layout.activity_main);
 
-        //final Animation animation = AnimationUtils.loadAnimation(this, R.anim.animation);
-        //final ImageView imageView = findViewById(R.id.imageView);
+        ImageView imageView = findViewById(R.id.imageView);
 
-        //imageView.startAnimation(animation);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        /*// On android sdk >= 11
-        ObjectAnimator animation = ObjectAnimator.ofFloat(imageView, "translationX", 100f);
-        animation.setDuration(1000);
-        animation.start();*/
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+                imageView.startAnimation(animation);
+
+                /*
+                // On android sdk >= 11
+                ObjectAnimator animation = ObjectAnimator.ofFloat(imageView, "translationX", 100f);
+                animation.setDuration(1000);
+                animation.start();
+                */
+            }
+        });
 
     }
 }
