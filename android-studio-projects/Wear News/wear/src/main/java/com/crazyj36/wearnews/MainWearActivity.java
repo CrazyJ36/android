34ab4f0a9af699b646252424ee9fbc7ec6c79226
CrazyJ36 <crazy_j36@hotmail.com>
2022-08-05 01:14:52 -0400

    u

--- android-studio-projects/Wear News/wear/src/main/java/com/crazyj36/wearnews/MainWearActivity.java
+++ android-studio-projects/Wear News/wear/src/main/java/com/crazyj36/wearnews/MainWearActivity.java
@@ -39,6 +39,7 @@ public class MainWearActivity extends FragmentActivity implements AmbientModeSup
         textClock = findViewById(R.id.textClock);
         new ArrayAdapter<>(getApplicationContext(), R.layout.recent_posts_list, recentPostsTitles);
         listView = findViewById(R.id.recentPostsListView);
+        // maybe? listView.setEnabled(false);
         ambientController = AmbientModeSupport.attach(this);
         setup();
     }
