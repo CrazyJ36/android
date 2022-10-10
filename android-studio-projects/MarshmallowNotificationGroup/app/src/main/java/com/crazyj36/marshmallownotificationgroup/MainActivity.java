c9ab3d0b572b083f8aa24393b27052c8c0c33f2e
CrazyJ36 <crazy_j36@hotmail.com>
2022-10-10 03:47:50 -0400

    u

---
+++ android-studio-projects/MarshmallowNotificationGroup/app/src/main/java/com/crazyj36/marshmallownotificationgroup/MainActivity.java
@@ -0,0 +1,45 @@
+package com.crazyj36.marshmallownotificationgroup;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, TestService.class);
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(intent);
+            }
+        });
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(intent);
+            }
+        });
+    }
+    public static void toast(Context context, String text) {
+        Handler mainHandler = new Handler(Looper.getMainLooper());
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ android-studio-projects/MarshmallowNotificationGroup/app/src/main/java/com/crazyj36/marshmallownotificationgroup/TestService.java
@@ -0,0 +1,73 @@
+package com.crazyj36.marshmallownotificationgroup;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class TestService extends Service {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NotificationCompat.Builder serviceNotificationBuilder = new NotificationCompat.Builder(this, "10")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("service running")
+                .setCategory(Notification.CATEGORY_SERVICE);
+
+        startForeground(10, serviceNotificationBuilder.build());
+        MainActivity.toast(getApplicationContext(), "service running");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Notification notification0 = new NotificationCompat.Builder(TestService.this, "0")
+                .setContentTitle("0")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        Notification notification1 = new NotificationCompat.Builder(TestService.this, "1")
+                .setContentTitle("1")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        Notification notification2 = new NotificationCompat.Builder(TestService.this, "2")
+                .setContentTitle("2")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        Notification notification3 = new NotificationCompat.Builder(TestService.this, "3")
+                .setContentTitle("3")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        Notification notification4 = new NotificationCompat.Builder(TestService.this, "4")
+                .setContentTitle("4")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        Notification notification5 = new NotificationCompat.Builder(TestService.this, "5")
+                .setContentTitle("5")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.notify(0, notification0);
+        notificationManager.notify(1, notification1);
+        notificationManager.notify(2, notification2);
+        notificationManager.notify(3, notification3);
+        notificationManager.notify(4, notification4);
+        notificationManager.notify(5, notification5);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+}
