c2a15bea485bfa06fd038a1be25015ddf6f27c05
CrazyJ36 <crazy_j36@hotmail.com>
2023-07-14 02:57:44 -0400

    u

--- android-studio-projects/Wear Phone Sync Tile 2/wear/src/main/java/com/crazyj36/wearphonesynctile/MyTileService.kt
+++ android-studio-projects/Wear Phone Sync Tile 2/wear/src/main/java/com/crazyj36/wearphonesynctile/MyTileService.kt
@@ -1,44 +1,31 @@
 package com.crazyj36.wearphonesynctile
 
-import android.app.admin.DeviceAdminInfo
-import android.content.Context
-import android.content.res.Resources
 import android.util.Log
 import android.widget.Toast
-import androidx.annotation.ColorInt
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 import androidx.wear.protolayout.ActionBuilders
 import androidx.wear.protolayout.ActionBuilders.Action
 import androidx.wear.protolayout.ActionBuilders.LoadAction
-import androidx.wear.protolayout.ColorBuilders
-import androidx.wear.protolayout.DeviceParametersBuilders
-import androidx.wear.protolayout.DeviceParametersBuilders.DeviceParameters
 import androidx.wear.protolayout.DimensionBuilders
 import androidx.wear.protolayout.LayoutElementBuilders
 import androidx.wear.protolayout.LayoutElementBuilders.Box
 import androidx.wear.protolayout.TimelineBuilders
 import androidx.wear.protolayout.LayoutElementBuilders.Layout
-import androidx.wear.protolayout.LayoutElementBuilders.LayoutElement
-import androidx.wear.protolayout.LayoutElementBuilders.Spacer
 import androidx.wear.protolayout.LayoutElementBuilders.VERTICAL_ALIGN_CENTER
 import androidx.wear.protolayout.ModifiersBuilders
 import androidx.wear.protolayout.ModifiersBuilders.Clickable
+import androidx.wear.protolayout.ModifiersBuilders.Modifiers
 import androidx.wear.protolayout.ResourceBuilders
 import androidx.wear.protolayout.TimelineBuilders.Timeline
 import androidx.wear.protolayout.material.Button
+import androidx.wear.protolayout.material.Text
 import androidx.wear.tiles.EventBuilders
 import androidx.wear.tiles.RequestBuilders.TileRequest
 import androidx.wear.tiles.RequestBuilders.ResourcesRequest
 import androidx.wear.tiles.TileBuilders.Tile
-import androidx.wear.protolayout.material.*
-import androidx.wear.protolayout.material.layouts.PrimaryLayout
 import androidx.wear.tiles.TileUpdateRequester
 import com.google.android.gms.wearable.*
 import com.google.android.gms.wearable.DataClient.OnDataChangedListener
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
-import com.google.android.horologist.compose.tools.buildDeviceParameters
 import com.google.android.horologist.tiles.SuspendingTileService
 import java.util.Timer
 import java.util.TimerTask
@@ -54,21 +41,11 @@ class MyTileService : SuspendingTileService(), OnDataChangedListener {
     private val timer = Timer()
     private val timerTask = MyTimerTask()
 
-    private val myClickable =  ModifiersBuilders.Modifiers.Builder()
-        .setClickable(Clickable.Builder()
-            .setId("clickId")
-            .setOnClick(
-                LoadAction.Builder().build()
-            ).build()
-        ).build().clickable.apply { Log.d("TILE", "log") }
-
     @ExperimentalHorologistApi
     override fun onCreate() {
         super.onCreate()
         timer.schedule(timerTask, 0, 3000)
     }
-
-
 
     @ExperimentalHorologistApi
     override fun onTileLeaveEvent(requestParams: EventBuilders.TileLeaveEvent) {
@@ -83,37 +60,39 @@ class MyTileService : SuspendingTileService(), OnDataChangedListener {
 
     @ExperimentalHorologistApi
     override suspend fun tileRequest(requestParams: TileRequest): Tile {
+        val text1: LayoutElementBuilders.Text = LayoutElementBuilders.Text.Builder()
+            .setText(message)
+            .build()
+        val text2: LayoutElementBuilders.Text = LayoutElementBuilders.Text.Builder()
+            .setText(timerMessage.toString())
+            .setModifiers(
+                ModifiersBuilders.Modifiers.Builder()
+                    .setPadding(
+                        ModifiersBuilders.Padding.Builder().setTop(DimensionBuilders.dp(16f)).build()
+                    ).build()
+            ).build()
+        val button: Button = Button.Builder(applicationContext,
+            Clickable.Builder()
+                .setId("buttonId")
+                .setOnClick(LoadAction.Builder().build())
+                .build()
+        ).build()
+        val box: Box = Box.Builder()
+            .setVerticalAlignment(VERTICAL_ALIGN_CENTER)
+            .setWidth(DimensionBuilders.expand())
+            .setHeight(DimensionBuilders.expand())
+            .addContent(text1).addContent(text2).addContent(button).build()
+
+        val newButton = Button.fromLayoutElement(box.contents[2])
+        newButton.let { Toast.makeText(applicationContext, "toast", Toast.LENGTH_SHORT).show() }
+        
         return Tile.Builder()
             .setResourcesVersion(RESOURCES_VERSION)
             .setTileTimeline(
                 Timeline.Builder().addTimelineEntry(
                     TimelineBuilders.TimelineEntry.Builder().setLayout(
                         Layout.Builder().setRoot(
-                            Box.Builder()
-                                .setVerticalAlignment(VERTICAL_ALIGN_CENTER)
-                                .setWidth(DimensionBuilders.expand())
-                                .setHeight(DimensionBuilders.expand())
-                                .addContent(
-                                    LayoutElementBuilders.Text.Builder().setText("tile").build()
-                                )
-                                .addContent(
-                                    LayoutElementBuilders.Text.Builder().setText("new text")
-                                        .setModifiers(
-                                            ModifiersBuilders.Modifiers.Builder()
-                                                .setPadding(
-                                                    ModifiersBuilders.Padding.Builder().setTop(DimensionBuilders.dp(16f)).build()
-                                            ).build()
-                                    ).build()
-                                )
-                                .addContent(
-                                    Button.Builder(
-                                        applicationContext,
-                                        Clickable.Builder()
-                                            .setId("buttonId")
-                                            .setOnClick(LoadAction.Builder().build())
-                                            .build()
-                                    ).build()
-                                ).build()
+                            box
                         ).build()
                     ).build()
                 ).build()
@@ -135,14 +114,13 @@ class MyTileService : SuspendingTileService(), OnDataChangedListener {
                         val dataMap: DataMap = DataMapItem.fromDataItem(dataItem).dataMap
                         // setting message or count here doesn't seem to get back to onTileRequest
                         message = dataMap.getInt("message").toString()
-                        //LoadAction.Builder()
-                        //Toast.makeText(this@MyTileService, "message: $message", Toast.LENGTH_SHORT).show()
+                        LoadAction.Builder()
+                        Toast.makeText(this@MyTileService, "message: $message", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
     }
-
 
     class MyTimerTask : TimerTask() {
         override fun run() {
