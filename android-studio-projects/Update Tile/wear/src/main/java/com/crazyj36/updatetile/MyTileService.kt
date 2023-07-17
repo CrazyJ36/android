b04c0757c8ac762d806b54dce9b498705e403a0f
CrazyJ36 <crazy_j36@hotmail.com>
2023-07-17 03:22:53 -0400

    updatetile: still doesnt work

--- android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
+++ android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
@@ -1,7 +1,9 @@
 package com.crazyj36.updatetile
 
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -9,32 +11,43 @@ import androidx.compose.ui.unit.sp
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.glance.Button
+import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
-import androidx.glance.LocalGlanceId
 import androidx.glance.background
 import androidx.glance.currentState
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
+import androidx.glance.layout.Row
 import androidx.glance.layout.padding
+import androidx.glance.semantics.contentDescription
+import androidx.glance.semantics.semantics
 import androidx.glance.state.PreferencesGlanceStateDefinition
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import androidx.glance.wear.tiles.GlanceTileService
+import androidx.glance.wear.tiles.action.ActionCallback
+import androidx.glance.wear.tiles.action.actionRunCallback
 import androidx.glance.wear.tiles.state.updateWearTileState
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import androidx.wear.tiles.ActionBuilders.LoadAction
+import kotlinx.coroutines.delay
+import java.util.concurrent.Executor
 
 private val prefsCountKey = intPreferencesKey("count")
+
 class MyTileService : GlanceTileService() {
     override val stateDefinition = PreferencesGlanceStateDefinition
+
     @Composable
     override fun Content() {
-        val currentCount = currentState<Preferences>()[prefsCountKey]
+        val currentCount = currentState<Preferences>()[prefsCountKey] ?: 0
         Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
+                modifier =
+                GlanceModifier.semantics({ contentDescription = "demo of actionRunCallback" })
         ) {
+
             Text(
                     text = currentCount.toString(),
                     style = TextStyle(
@@ -43,27 +56,32 @@ class MyTileService : GlanceTileService() {
                             fontSize = 20.sp
                     )
             )
-            Button(
-                    text = "+1",
-                    modifier = GlanceModifier.padding(5.dp).background(Color.Cyan),
-                    onClick = {
-                        Toast.makeText(applicationContext, "clicked", Toast.LENGTH_SHORT).show()
-                    }
-            )
-            val glanceId = LocalGlanceId.current
-            Thread {
-                for (i in 1..5) {
-                    Log.d("UPDATETILE", "running")
-                    MainScope().launch {
-                        updateWearTileState(applicationContext, PreferencesGlanceStateDefinition, glanceId) { preferences ->
-                            preferences.toMutablePreferences().apply {
-                                set(prefsCountKey, (this[prefsCountKey] ?: 0) + 1)
-                            }
-                        }
-                    }
-                    Thread.sleep(1000)
-                }
-            }.start()
+
+            Row(
+                    horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                        text = "+",
+                        modifier = GlanceModifier.padding(5.dp).background(Color.Blue),
+                        onClick = actionRunCallback<ClickAddAction>()
+                )
+            }
         }
     }
 }
+class ClickAddAction : ActionCallback {
+    override suspend fun onAction(
+            context: Context,
+            glanceId: GlanceId
+    ) {
+        for (i in 1..5) {
+            updateWearTileState(context, PreferencesGlanceStateDefinition, glanceId) { prefs ->
+                prefs.toMutablePreferences().apply {
+                    set(prefsCountKey, (this[prefsCountKey] ?: 0) + 1)
+                }
+            }
+            delay(1000)
+        }
+    }
+}
+
