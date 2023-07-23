f945344174b901b31df1d43d18dc0cc06bc7df2a
CrazyJ36 <crazy_j36@hotmail.com>
2023-07-23 05:27:22 -0400

    u

--- android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MainActivity.kt
+++ android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MainActivity.kt
@@ -1,45 +1,25 @@
 package com.crazyj36.updatetile
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.wear.compose.material.Text
 
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val myTileService = MyTileService
-
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
             ) {
-                AndroidView(
-                    factory = { context ->
-                        TextView(context).apply {
-                            text = "Android View"
-                        }
-                    }
-                )
-                AndroidView(
-                    factory = { context ->
-                        Button(context).apply {
-                            text = "Button"
-                            setOnClickListener {
-                                //Toast.makeText(context, "view toast", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                )
+                Text(
+                    modifier = Modifier.align(Alignment.Center),
+                    text = getString(R.string.mainActivityText))
             }
         }
     }
