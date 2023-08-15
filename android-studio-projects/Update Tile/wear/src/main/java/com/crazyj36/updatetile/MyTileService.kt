44145697d075b3bc2202f6ab4fbbc9ce46673d36
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-15 17:55:47 -0400

    u

--- android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
+++ android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
@@ -1,35 +1,25 @@
 package com.crazyj36.updatetile
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.hardware.SensorManager
+import android.content.Context
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.wear.protolayout.ActionBuilders
 import androidx.wear.protolayout.ResourceBuilders
-import androidx.wear.protolayout.StateBuilders
 import androidx.wear.protolayout.TimelineBuilders
-import androidx.wear.protolayout.TypeBuilders
-import androidx.wear.protolayout.TypeBuilders.StringLayoutConstraint
-import androidx.wear.protolayout.expression.AppDataKey
 import androidx.wear.protolayout.expression.DynamicBuilders
-import androidx.wear.protolayout.expression.DynamicBuilders.DynamicString
-import androidx.wear.protolayout.expression.DynamicDataBuilders
-import androidx.wear.protolayout.expression.PlatformHealthSources
 import androidx.wear.protolayout.material.Text
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders
 import androidx.wear.tiles.TileService
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import java.util.Timer
+import java.util.TimerTask
 
 const val RESOURCES_VERSION = "1"
 
 class MyTileService: TileService() {
+    var context: Context? = null
+
     companion object {
         var count = 0
     }
@@ -50,14 +40,15 @@ class MyTileService: TileService() {
     override fun onCreate() {
         super.onCreate()
         val systemTime = DynamicBuilders.DynamicInstant.platformTimeWithSecondsPrecision()
-        for (i in 0..4) {
-            Toast.makeText(this, "updated", Toast.LENGTH_SHORT).show()
+        Timer().schedule(MyTimerTask(applicationContext), 3000)
+    }
+    class MyTimerTask(private val context: Context) : TimerTask() {
+        override fun run() {
+            Toast.makeText(context, "updated", Toast.LENGTH_SHORT).show()
             count++
             ActionBuilders.LoadAction.Builder().build()
-            MainScope().launch {
-                delay(3000)
-            }
         }
+
     }
     override fun onTileRequest(requestParams: RequestBuilders.TileRequest): ListenableFuture<TileBuilders.Tile> {
         return Futures.immediateFuture(
