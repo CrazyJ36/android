61a339aea7e31897a521968ffed0ea917adaec07
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-20 21:55:24 -0400

    testing track health events

--- android-studio-projects/HeathEventTracker/app/src/main/java/com/example/heatheventtracker/presentation/MainActivity.kt
+++ android-studio-projects/HeathEventTracker/app/src/main/java/com/example/heatheventtracker/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ package com.example.heatheventtracker.presentation
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -16,13 +17,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.health.services.client.HealthServices
-import androidx.health.services.client.HealthServicesClient
 import androidx.health.services.client.MeasureCallback
 import androidx.health.services.client.MeasureClient
 import androidx.health.services.client.data.Availability
@@ -30,15 +29,10 @@ import androidx.health.services.client.data.DataPointContainer
 import androidx.health.services.client.data.DataType
 import androidx.health.services.client.data.DataTypeAvailability
 import androidx.health.services.client.data.DeltaDataType
-import androidx.health.services.client.data.MeasureCapabilities
-import androidx.lifecycle.lifecycleScope
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import kotlinx.coroutines.guava.await
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     lateinit var measureClient: MeasureClient
@@ -64,20 +58,36 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun WearApp() {
         var text by remember { mutableStateOf("") }
+        var showAvailability = true
         val heartRateCallback = object: MeasureCallback {
             override fun onAvailabilityChanged(
                 dataType: DeltaDataType<*, *>,
                 availability: Availability
             ) {
-                if (availability is DataTypeAvailability) {
-                    text = "heart rate available."
-                } else {
-                    text = "can't get heart rate."
+                if (showAvailability) {
+                    if (availability is DataTypeAvailability) {
+                        text = "getting heartrate..."
+                        Toast.makeText(
+                            applicationContext,
+                            "heart rate available on device.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        text = "failed"
+                        Toast.makeText(
+                            applicationContext,
+                            "can't get heart rate from device.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    showAvailability = false
                 }
             }
             override fun onDataReceived(data: DataPointContainer) {
-                text = data.getData(DataType.HEART_RATE_BPM)
-                    .last().value.toString()
+                if (text != "failed") {
+                        text = data.getData(DataType.HEART_RATE_BPM)
+                            .last().value.toString()
+                }
             }
         }
         if (applicationContext.checkSelfPermission(
