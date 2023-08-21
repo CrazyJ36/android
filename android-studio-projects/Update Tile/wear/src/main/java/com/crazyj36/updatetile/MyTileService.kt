a2bcf87351d8412bf9f35628aa3312bc074f0976
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-21 01:51:39 -0400

    update tile work

--- android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
+++ android-studio-projects/Update Tile/wear/src/main/java/com/crazyj36/updatetile/MyTileService.kt
@@ -56,7 +56,7 @@ class MyTileService : TileService() {
                         "need body sensor permission")
                 } else {
                     val heartRate: String =
-                        String.format("%f",PlatformHealthSources
+                        String.format("%.2f",PlatformHealthSources
                             .heartRateBpm())
                     Log.d("UPDATETILE",
                         "HEART RATE: $heartRate"
