2a52eac8e0beeae4a83ff44bee38873f497b0399
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-26 17:39:37 -0400

    learning and testing watch face development

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/CustomCanvasRenderer.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/CustomCanvasRenderer.kt
@@ -90,7 +90,7 @@ class CustomCanvasRenderer(
             darkPaint
         )*/
         val rect = RectF(
-            (width - (width / 4) * 3).toFloat(),
+            (width - (width / 8) * 3).toFloat(),
             (height - 200).toFloat(),
             (width - 20).toFloat(),
             (height - 20).toFloat()
