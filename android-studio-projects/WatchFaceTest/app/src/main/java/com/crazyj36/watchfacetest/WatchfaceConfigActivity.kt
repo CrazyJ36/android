19b3f799d0acce3eb0149f9fe8879f2048f65ebb
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-29 21:23:37 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/WatchfaceConfigActivity.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/WatchfaceConfigActivity.kt
@@ -2,6 +2,7 @@ package com.crazyj36.watchfacetest
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.wear.watchface.editor.EditorSession
 import kotlinx.coroutines.CoroutineScope
@@ -10,15 +11,24 @@ import kotlinx.coroutines.launch
 
 class WatchFaceConfigActivity: ComponentActivity() {
     private lateinit var scope : CoroutineScope
-    private lateinit var editorSession: EditorSession
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+    lateinit var editorSession: EditorSession
+    override fun onCreate(savedInstanceState: Bundle?,
+                          persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
+        Log.d("WATCHFACETEST", "WatcchFaceConfigActivity opened")
 
         scope.launch(Dispatchers.Main.immediate) {
+            editorSession =
+                EditorSession.createOnWatchEditorSession(
+                    this@WatchFaceConfigActivity
+                )
             editorSession
                 .openComplicationDataSourceChooser(1)
-
         }
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        editorSession.close()
+    }
+
 }
