aef0c77a4e289375cbb85a55edcfcf8019c0094d
CrazyJ36 <crazy_j36@hotmail.com>
2023-08-30 22:47:21 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/WatchFaceConfigActivity.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/WatchFaceConfigActivity.kt
@@ -12,26 +12,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.wear.watchface.editor.EditorSession
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class WatchFaceConfigActivity: ComponentActivity() {
     private lateinit var editorSession: EditorSession
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (checkSelfPermission(
-                "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"
-            ) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(
-            arrayOf(
-                "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"),
-            0
-            )
-            mainConfig()
-        } else {
-            mainConfig()
-        }
-    }
-    private fun mainConfig() {
         setContent {
             Box(modifier = Modifier.fillMaxSize())
         }
@@ -41,6 +28,7 @@ class WatchFaceConfigActivity: ComponentActivity() {
             Toast.LENGTH_LONG
         ).show()
         MainScope().launch(Dispatchers.Main.immediate) {
+            delay(2000)
             editorSession = EditorSession.createOnWatchEditorSession(
                 this@WatchFaceConfigActivity
             )
