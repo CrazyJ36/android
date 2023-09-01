685e462c79deda7a777b53a20b0f0b9ca773ed91
CrazyJ36 <crazy_j36@hotmail.com>
2023-09-01 04:34:39 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
@@ -1,26 +1,24 @@
 package com.crazyj36.watchfacetest
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.fragment.app.FragmentActivity
-import androidx.wear.activity.ConfirmationActivity
 
 class GetComplicationPermission : ComponentActivity() {
-    val requestPermission = registerForActivityResult(
+    private val requestPermission = registerForActivityResult(
         ActivityResultContracts.RequestPermission()) { isGranted ->
         if (isGranted) Log.d("WATCHFACETEST", "granted in registerForActivityResult.")
         else Log.d("WATCHFACETEST", "not granted in registerForActivityResult")
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
                 /*if (getSharedPreferences("file_show_complication_warning",
                         Context.MODE_PRIVATE).getBoolean("showComplicationWarning", true)) {*/
                    /* val intent = Intent(
