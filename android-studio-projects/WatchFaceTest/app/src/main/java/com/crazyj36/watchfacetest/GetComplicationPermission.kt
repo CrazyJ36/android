d1f47120b696139b21af09b95b97157024107275
CrazyJ36 <crazy_j36@hotmail.com>
2023-09-01 04:40:34 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
@@ -1,51 +1,54 @@
 package com.crazyj36.watchfacetest
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.wear.activity.ConfirmationActivity
 
 class GetComplicationPermission : ComponentActivity() {
     private val requestPermission = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()) { isGranted ->
-        if (isGranted) Log.d("WATCHFACETEST", "granted in registerForActivityResult.")
-        else Log.d("WATCHFACETEST", "not granted in registerForActivityResult")
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            if (getSharedPreferences(
+                    "file_show_complication_warning",
+                    Context.MODE_PRIVATE
+                ).getBoolean("showComplicationWarning", true)
+            ) {
+                val intent = Intent(
+                    this,
+                    ConfirmationActivity::class.java
+                ).apply {
+                    putExtra(
+                        ConfirmationActivity
+                            .EXTRA_ANIMATION_TYPE, ConfirmationActivity
+                            .SUCCESS_ANIMATION
+                    )
+                    putExtra(
+                        ConfirmationActivity
+                            .EXTRA_MESSAGE,
+                        resources.getString(R.string.complicationWarningText)
+                    )
+                    putExtra(
+                        ConfirmationActivity.EXTRA_ANIMATION_DURATION_MILLIS,
+                        3500
+                    )
+                }
+                startActivity(intent)
+                getSharedPreferences("file_show_complication_warning", Context.MODE_PRIVATE)
+                    .edit().putBoolean(
+                        "showComplicationWarning", false
+                    ).apply()
+            }
+        } else {
+            finish()
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-                /*if (getSharedPreferences("file_show_complication_warning",
-                        Context.MODE_PRIVATE).getBoolean("showComplicationWarning", true)) {*/
-                   /* val intent = Intent(
-                        this,
-                        ConfirmationActivity::class.java
-                    ).apply {
-                        putExtra(
-                            ConfirmationActivity
-                                .EXTRA_ANIMATION_TYPE, ConfirmationActivity
-                                .SUCCESS_ANIMATION
-                        )
-                        putExtra(
-                            ConfirmationActivity
-                                .EXTRA_MESSAGE,
-                            resources.getString(R.string.complicationWarningText)
-                        )
-                        putExtra(
-                            ConfirmationActivity.EXTRA_ANIMATION_DURATION_MILLIS,
-                            3500
-                        )
-                    }
-                    startActivity(intent)
-                    /*getSharedPreferences("file_show_complication_warning", Context.MODE_PRIVATE)
-                        .edit().putBoolean(
-                            "showComplicationWarning", false
-                        ).apply()*/
-                //}*/
-        }
-
+        requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
+    }
 }
