24838f993452c8fe8f1bbc5316b4a19f6d7dcab1
CrazyJ36 <crazy_j36@hotmail.com>
2023-09-01 01:44:40 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
@@ -1,12 +1,12 @@
 package com.crazyj36.watchfacetest
 
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.wear.activity.ConfirmationActivity
 
 class GetComplicationPermission: ComponentActivity() {
@@ -16,57 +16,51 @@ class GetComplicationPermission: ComponentActivity() {
                 .RequestPermission()
         ) { isGranted: Boolean ->
             if (isGranted) {
-                //GetComplicationPermission().isGranted = true
-                /*if (getSharedPreferences(
-                        "file_show_complication_warning",
-                        Context.MODE_PRIVATE
-                    ).getBoolean("showComplicationWarning", true)
-                ) {*/
-
-                //}
                 Log.d("WATCHFACETEST", "granted in activityResult")
             } else {
                 Log.d("WATCHFACETEST", "not granted in activityResult.")
-                //finish()
             }
         }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestPermissionLauncher.launch(
-            "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"
-        )
         when {
             checkSelfPermission(
                 "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"
             ) == PackageManager.PERMISSION_GRANTED -> {
-                val intent = Intent(
-                    this,
-                    ConfirmationActivity::class.java
-                ).apply {
-                    putExtra(
-                        ConfirmationActivity
-                            .EXTRA_ANIMATION_TYPE, ConfirmationActivity
-                            .SUCCESS_ANIMATION
-                    )
-                    putExtra(
-                        ConfirmationActivity
-                            .EXTRA_MESSAGE,
-                        resources.getString(R.string.complicationWarningText)
-                    )
-                    putExtra(ConfirmationActivity.EXTRA_ANIMATION_DURATION_MILLIS,
-                        3500)
+                if (getSharedPreferences(
+                        "file_show_complication_warning",
+                        Context.MODE_PRIVATE
+                    ).getBoolean("showComplicationWarning", true)
+                ) {
+                    val intent = Intent(
+                        this,
+                        ConfirmationActivity::class.java
+                    ).apply {
+                        putExtra(
+                            ConfirmationActivity
+                                .EXTRA_ANIMATION_TYPE, ConfirmationActivity
+                                .SUCCESS_ANIMATION
+                        )
+                        putExtra(
+                            ConfirmationActivity
+                                .EXTRA_MESSAGE,
+                            resources.getString(R.string.complicationWarningText)
+                        )
+                        putExtra(
+                            ConfirmationActivity.EXTRA_ANIMATION_DURATION_MILLIS,
+                            3500
+                        )
+                    }
+                    startActivity(intent)
+                    getSharedPreferences("file_show_complication_warning", Context.MODE_PRIVATE)
+                        .edit().putBoolean("showComplicationWarning", false).apply()
+                    Log.d("WATCHFACETEST", "granted in onCreate()")
                 }
-                startActivity(intent)
-                /*getSharedPreferences("file_show_complication_warning", Context.MODE_PRIVATE)
-                    .edit().putBoolean("showComplicationWarning", false).apply()
-
-                 */
-                Log.d("WATCHFACETEST", "granted in onCreate()")
             } else -> {
+            requestPermissionLauncher.launch(
+                "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"
+            )
                 Log.d("WATCHFACETEST", "not granted in onCreate()")
-                //requestPermissionLauncher.launch(
-                //    "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA"
-                //)
             }
         }
     }
