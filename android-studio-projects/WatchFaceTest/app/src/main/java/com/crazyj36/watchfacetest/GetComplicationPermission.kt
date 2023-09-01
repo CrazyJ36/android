c8025e273473b2e99ca107d84a086835362474b9
CrazyJ36 <crazy_j36@hotmail.com>
2023-09-01 04:31:26 -0400

    watchfacetest work

--- android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
+++ android-studio-projects/WatchFaceTest/app/src/main/java/com/crazyj36/watchfacetest/GetComplicationPermission.kt
@@ -11,18 +11,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.FragmentActivity
 import androidx.wear.activity.ConfirmationActivity
 
-class GetComplicationPermission : FragmentActivity() {
+class GetComplicationPermission : ComponentActivity() {
+    val requestPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) Log.d("WATCHFACETEST", "granted in registerForActivityResult.")
+        else Log.d("WATCHFACETEST", "not granted in registerForActivityResult")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val requestPermission = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) Log.d("WATCHFACETEST", "granted in registerForActivityResult.")
-            else Log.d("WATCHFACETEST", "not granted in registerForActivityResult")
-        }
-        when (checkSelfPermission(
-            "com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")) {
-            PackageManager.PERMISSION_GRANTED -> {
+        requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
                 /*if (getSharedPreferences("file_show_complication_warning",
                         Context.MODE_PRIVATE).getBoolean("showComplicationWarning", true)) {*/
                    /* val intent = Intent(
@@ -50,11 +48,6 @@ class GetComplicationPermission : FragmentActivity() {
                             "showComplicationWarning", false
                         ).apply()*/
                 //}*/
-                Log.d("WATCHFACETEST", "granted in onCreate()")
-            } PackageManager.PERMISSION_DENIED -> {
-                Log.d("WATCHFACETEST", "not granted in onCreate()")
-                requestPermission.launch("com.google.android.wearable.permission.RECEIVE_COMPLICATION_DATA")
-            }
         }
-    }
+
 }
