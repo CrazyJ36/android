5d513d982829eb6c256205f3f76e48a63bede319
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-01 03:49:03 -0500

    u

--- android-studio-projects/CustomComplicationDataSourceTest/app/src/main/java/com/crazyj36/customcomplicationdatasourcetest/CustomComplicationDataSourceService.kt
+++ android-studio-projects/CustomComplicationDataSourceTest/app/src/main/java/com/crazyj36/customcomplicationdatasourcetest/CustomComplicationDataSourceService.kt
@@ -15,7 +15,13 @@ import java.util.TimerTask
 
 class CustomComplicationDataSourceService: SuspendingComplicationDataSourceService() {
     private var counter: Int = 0
-    private lateinit var timer: Timer
+    /*private lateinit var timer: Timer
+    private lateinit var timerTask: TimerTask
+    val updater = ComplicationDataSourceUpdateRequester.create(
+    this, ComponentName(
+            this, javaClass)
+    )*/
+
 
     override fun getPreviewData(type: ComplicationType): ComplicationData {
         return ShortTextComplicationData.Builder(
@@ -25,38 +31,36 @@ class CustomComplicationDataSourceService: SuspendingComplicationDataSourceServi
             contentDescription = PlainComplicationText.Builder(
                 getString(R.string.shortTextComplicationContentDescription)
             ).build()
-        ).setTitle(PlainComplicationText.Builder(
-            getString(R.string.complicationTitleText)).build()).build()
+        ).setTitle(
+            PlainComplicationText.Builder(
+                getString(R.string.complicationTitleText)
+            ).build()
+        ).build()
     }
 
     override fun onComplicationActivated(complicationInstanceId: Int, type: ComplicationType) {
         super.onComplicationActivated(complicationInstanceId, type)
         Log.d("CRAZYJ36", "complication activated")
-        val request = ComplicationDataSourceUpdateRequester.create(
-            applicationContext, ComponentName(
-                applicationContext, this::class.java
-            )
-        )
-        timer = Timer()
-        timer.schedule(object: TimerTask() {
-            override fun run() {
-                Log.d("CRAZYJ36", "timer running")
-                counter++
-                request.requestUpdate()
-            }
-
-        }, 0, 1000)
+        counter = 0
+        /*timer = Timer()
+        timerTask = MyTimerTask()
+        timer.schedule(timerTask, 0, 1000)*/
     }
 
     override fun onComplicationDeactivated(complicationInstanceId: Int) {
         super.onComplicationDeactivated(complicationInstanceId)
         Log.d("CRAZYJ36", "complication deactivated")
-        timer.cancel()
-        timer.purge()
+        /*if (this::timer.isInitialized) {
+            timerTask.cancel()
+            timer.cancel()
+            timer.purge()
+        }*/
     }
+
     override suspend fun onComplicationRequest(
-        request: ComplicationRequest): ComplicationData {
-        Log.d("CRAZYJ36","updating complication")
+        request: ComplicationRequest
+    ): ComplicationData {
+        Log.d("CRAZYJ36", "updating complication")
         return ShortTextComplicationData.Builder(
             text = PlainComplicationText.Builder(
                 counter.toString()
@@ -64,7 +68,24 @@ class CustomComplicationDataSourceService: SuspendingComplicationDataSourceServi
             contentDescription = PlainComplicationText.Builder(
                 getString(R.string.shortTextComplicationContentDescription)
             ).build()
-        ).setTitle(PlainComplicationText.Builder(
-            getString(R.string.complicationTitleText)).build()).build()
+        ).setTitle(
+            PlainComplicationText.Builder(
+                getString(R.string.complicationTitleText)
+            ).build()
+        ).build()
+    }
+
+    inner class MyTimerTask : TimerTask() {
+        override fun run() {
+            /*if (counter < 10) {
+                Log.d("CRAZYJ36", "timer running")
+                counter++
+                updater.requestUpdateAll()
+            } else {
+                timerTask.cancel()
+                timer.cancel()
+                timer.purge()
+            }*/
+        }
     }
 }
