0591993b5ffacda2b383a87117c3e5ec7c14914f
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-03 23:45:48 -0500

    u

--- android-studio-projects/GetSpotifyFollowerCount/app/src/main/java/com/crazyj36/getspotifyfollowercount/MainActivity.kt
+++ android-studio-projects/GetSpotifyFollowerCount/app/src/main/java/com/crazyj36/getspotifyfollowercount/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.crazyj36.getspotifyfollowercount
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
@@ -14,17 +15,16 @@ import androidx.compose.ui.Modifier
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.Call
-import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.Response
 import okio.IOException
-import org.json.JSONException
-import org.json.JSONObject
 
 
-class MainActivity : ComponentActivity(), Callback {
+class MainActivity : ComponentActivity() {
     private var artistInfoString = mutableStateOf("waiting")
     private var okHttpCall: Call? = null
     private val requestTokenLauncher = registerForActivityResult(
@@ -40,8 +40,18 @@ class MainActivity : ComponentActivity(), Callback {
                     "Authorization",
                     "Bearer ${response.accessToken}"
                 ).build()
-            okHttpCall = okHttpClient.newCall(request)
-            okHttpCall!!.enqueue(this)
+            CoroutineScope(Dispatchers.IO).launch {
+                artistInfoString.value =
+                    okHttpCall!!.execute().body.toString()
+                okHttpCall = okHttpClient.newCall(request)
+                runOnUiThread() {
+                    Toast.makeText(
+                        applicationContext,
+                        artistInfoString.value,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         } catch (exception: IOException) {
             Toast.makeText(
                 applicationContext,
@@ -67,32 +77,13 @@ class MainActivity : ComponentActivity(), Callback {
                 "19260f6162744ecc8719814edceec27e",
                 AuthorizationResponse.Type.TOKEN,
                 Uri.parse("http://www.crazyj36.rocks/development/web/index.html").toString()
-            ).setScopes(arrayOf("streaming"))
-                .setShowDialog(true).build()
+            ).setScopes(arrayOf("streaming")).build()
         requestTokenLauncher.launch(
             AuthorizationClient.createLoginActivityIntent(
                 this,
                 builder
             )
         )
-    }
-
-    override fun onFailure(call: Call, e: java.io.IOException) {
-
-    }
-
-    override fun onResponse(call: Call, response: Response) {
-        try {
-            val jsonObject =
-                JSONObject(response.body!!.string())
-            Toast.makeText(applicationContext, jsonObject.toString(),
-                Toast.LENGTH_SHORT).show()
-            artistInfoString.value = jsonObject.toString()
-        } catch (jsonException: JSONException) {
-            Toast.makeText(applicationContext,
-                "Json Exception in response",
-                Toast.LENGTH_SHORT).show()
-        }
     }
     override fun onDestroy() {
         super.onDestroy()
