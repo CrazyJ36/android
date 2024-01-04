08ae525d0a2d7803b77cb28ad54a9e277497218f
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-04 01:03:20 -0500

    u

--- android-studio-projects/GetSpotifyFollowerCount/app/src/main/java/com/crazyj36/getspotifyfollowercount/MainActivity.kt
+++ android-studio-projects/GetSpotifyFollowerCount/app/src/main/java/com/crazyj36/getspotifyfollowercount/MainActivity.kt
@@ -32,13 +32,21 @@ import org.json.JSONObject
 class MainActivity : ComponentActivity(), Callback {
     private var artistInfoString = mutableStateOf("waiting")
     private var okHttpCall: Call? = null
-    private var accessToken: String? = null
     private val requestTokenLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) {
-        accessToken = AuthorizationClient.getResponse(
+        Log.d("MYLOG", "started activity")
+        val accessToken = AuthorizationClient.getResponse(
             it.resultCode, it.data).accessToken
-        Log.d("TEST", "got token")
+        okHttpCall = OkHttpClient().newCall(
+            Request.Builder()
+                .url("https://api.spotify.com/v1/artists/02UTIVsX3sxUEjvIONrzFe")
+                .addHeader(
+                    "Authorization",
+                    "Bearer ${accessToken}"
+                ).build()
+        )
+        okHttpCall!!.enqueue(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,41 +64,27 @@ class MainActivity : ComponentActivity(), Callback {
             AuthorizationRequest.Builder(
                 "19260f6162744ecc8719814edceec27e",
                 AuthorizationResponse.Type.TOKEN,
-                "http://localhost:8888/callback"
-            ).setScopes(arrayOf("streaming")).build()
+                "http://www.crazyj36.rocks/development/web/index.html"
+            ).setScopes(arrayOf("streaming"))
         requestTokenLauncher.launch(
             AuthorizationClient.createLoginActivityIntent(
                 this,
-                builder
+                builder.build()
             )
         )
-
-        CoroutineScope(Dispatchers.Main).launch {
-            while (accessToken == null) {
-                Log.d("TEST", "getting token")
-                delay(1000)
-            }
-        }
-        val okHttpClient = OkHttpClient()
-        val request = Request.Builder()
-            .url("https://api.spotify.com/v1/artists/02UTIVsX3sxUEjvIONrzFe")
-            .addHeader(
-                "Authorization",
-                "Bearer ${accessToken}"
-            ).build()
-        okHttpCall = okHttpClient.newCall(request)
-        okHttpCall!!.enqueue(this)
     }
 
+    override fun onFailure(call: Call, e: java.io.IOException) {
+        Log.d("MYLOG", "failed response")
+    }
+
+    override fun onResponse(call: Call, response: Response) {
+        Log.d("MYLOG", "got response")
+        artistInfoString.value = JSONObject(response.body!!.string()).toString()
+    }
     override fun onDestroy() {
         super.onDestroy()
         if (okHttpCall != null) okHttpCall!!.cancel()
     }
 
-    override fun onFailure(call: Call, e: java.io.IOException) {
-    }
-
-    override fun onResponse(call: Call, response: Response) {
-        artistInfoString.value = JSONObject(response.body!!.string()).toString()
-    }
 }
