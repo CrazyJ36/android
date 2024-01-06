31da6642e4234579e20105290938fb92995fdd9c
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-06 05:27:00 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -1,11 +1,13 @@
 package com.crazyj36.complicationtest
 
 import android.content.Context
+import android.graphics.BlendMode
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.graphics.Rect
 import android.graphics.RectF
+import android.os.Build
 import android.view.SurfaceHolder
 import androidx.wear.watchface.ComplicationSlotsManager
 import androidx.wear.watchface.Renderer
@@ -50,6 +52,7 @@ class WatchFaceCanvasRenderer(
             MyWatchFaceService.complicationDrawable.setContext(myContext)
             MyWatchFaceService.complicationDrawable.setTint(Color.WHITE)
             MyWatchFaceService.complicationDrawable.setTintMode(PorterDuff.Mode.MULTIPLY)
+            if (Build.VERSION.SDK_INT >= 29) MyWatchFaceService.complicationDrawable.setTintBlendMode(BlendMode.SRC_ATOP)
             MyWatchFaceService.complicationDrawable.setBounds(100, 100, 200, 200)
             MyWatchFaceService.complicationDrawable.draw(canvas)
             //complication.render(canvas, zonedDateTime, renderParameters)
