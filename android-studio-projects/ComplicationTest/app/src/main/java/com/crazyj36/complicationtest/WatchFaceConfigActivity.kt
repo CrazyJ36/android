ed36795105ec2d1fac72d82d8c393e4e1dccc1ed
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-06 23:42:58 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceConfigActivity.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceConfigActivity.kt
@@ -6,8 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.lifecycleScope
 import androidx.wear.watchface.DrawMode
 import androidx.wear.watchface.RenderParameters
