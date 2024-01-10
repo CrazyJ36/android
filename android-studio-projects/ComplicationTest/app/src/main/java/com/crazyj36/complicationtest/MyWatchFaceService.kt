7efdb8099ff095359a0e1cc0c581aa42312a7e3f
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-10 05:07:00 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/MyWatchFaceService.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/MyWatchFaceService.kt
@@ -2,7 +2,6 @@ package com.crazyj36.complicationtest
 
 import android.graphics.RectF
 import android.view.SurfaceHolder
-import android.widget.Toast
 import androidx.wear.watchface.CanvasComplicationFactory
 import androidx.wear.watchface.CanvasType
 import androidx.wear.watchface.ComplicationSlot
