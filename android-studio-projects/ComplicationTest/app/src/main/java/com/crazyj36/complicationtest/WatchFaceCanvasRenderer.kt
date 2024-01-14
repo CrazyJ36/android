1658b97d26500d79c854516838d22737532ef229
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-14 17:20:27 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -277,7 +277,7 @@ class WatchFaceCanvasRenderer(
                 ComplicationData.Companion.IMAGE_STYLE_ICON) {
                 Log.d(tag, "dataSourceSmallImage is type ICON, coloring...")
 
-                smallImageComplicationData!!.smallImage.image.loadDrawable(context)!!.apply {
+                smallImageComplicationData!!.smallImage.image.loadDrawable(context)!!.mutate().apply {
                     colorFilter = ColorMatrixColorFilter(colorMatrix)
                     setTintBlendMode(BlendMode.COLOR_BURN)
                     setTint(Color.RED)
