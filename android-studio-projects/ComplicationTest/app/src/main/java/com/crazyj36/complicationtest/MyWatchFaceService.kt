c424a5cb29dce3cdee196369bcb69510d7cdb6c7
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-15 01:17:38 -0500

    good progress

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/MyWatchFaceService.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/MyWatchFaceService.kt
@@ -27,8 +27,6 @@ class MyWatchFaceService : WatchFaceService() {
     ): ComplicationSlotsManager {
         val complicationId = 0
         val supportedTypes = listOf(
-            ComplicationType.NOT_CONFIGURED,
-            ComplicationType.EMPTY,
             ComplicationType.SHORT_TEXT,
             ComplicationType.SMALL_IMAGE
         )
