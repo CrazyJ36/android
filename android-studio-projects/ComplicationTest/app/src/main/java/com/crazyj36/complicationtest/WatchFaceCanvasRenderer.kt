c34126c9fa09f8db2ca007b8d27466345fb226f4
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-15 23:22:41 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -325,7 +325,7 @@ class WatchFaceCanvasRenderer(
                         clearColorFilter()
                         //colorFilter = ColorMatrixColorFilter(colorMatrix)
                     }.toBitmap().toIcon()
-                }.setTintBlendMode(BlendMode.COLOR).setTint(Color.RED)
+                }.setTintBlendMode(BlendMode.MULTIPLY).setTint(Color.RED)
             }
         }
         if (complicationWireData!!.hasBurnInProtectionSmallImage()) {
@@ -338,7 +338,7 @@ class WatchFaceCanvasRenderer(
                         clearColorFilter()
                         //colorFilter = ColorMatrixColorFilter(colorMatrix)
                     }.toBitmap().toIcon()
-                }.setTintBlendMode(BlendMode.COLOR).setTint(Color.RED)
+                }.setTintBlendMode(BlendMode.MULTIPLY).setTint(Color.RED)
             }
         }
         if (complicationWireData!!.hasLargeImage()) {
