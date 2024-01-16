1a0a6c81842b7d7d3b85665007ea706f3a7b5f90
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-15 19:16:17 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -71,8 +71,8 @@ class WatchFaceCanvasRenderer(
     private val paint = Paint()
     private val colorMatrix = floatArrayOf(
         1f, 1f, 1f, 0f, 0f,
-        0f, 1f, 0f, 0f, 0f,
-        0f, 0f, 1f, 0f, 0f,
+        -1f, -1f, -1f, 0f, 255f,
+        -1f, -1f, -1f, 0f, 255f,
         0f, 0f, 0f, 1f, 0f
     )
     override fun renderHighlightLayer(
