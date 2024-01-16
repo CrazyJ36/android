1fcf5b5de1d72cc68b0a58d26137a8dd3c38ddf3
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-16 02:16:25 -0500

    progress 3

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -69,7 +69,7 @@ class WatchFaceCanvasRenderer(
     private var dataSourceDynamicValues: DynamicBuilders.DynamicFloat? = null
     private val paint = Paint()
     private val colorMatrix = floatArrayOf(
-        -1f, 0f, 0f, 0f, 0f,
+        -0.50f, 0f, 0f, 0f, 0f,
         0f, 1f, 0f, 0f, 0f,
         0f, 0f, 1f, 0f, 0f,
         0f, 0f, 0f, 1f, 0f
