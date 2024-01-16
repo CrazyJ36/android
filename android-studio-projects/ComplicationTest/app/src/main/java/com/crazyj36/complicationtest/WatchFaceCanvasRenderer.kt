54cdc3ffd97a6d4b622cb42c7296b190fcd30bdc
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-16 03:39:50 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -327,13 +327,13 @@ class WatchFaceCanvasRenderer(
 
             if (dataSourceSmallImage!!.type == ComplicationData.IMAGE_STYLE_ICON) {
                 Log.d(tag, "ComplicationType is IMAGE_STYLE_ICON")
-                val mColorFilter = PorterDuffColorFilter(Color.argb(1f, 1f, 0f, 0f), PorterDuff.Mode.MULTIPLY)
+                val mColorFilter = PorterDuffColorFilter(Color.argb(1f, 1f, 1f, 1f), PorterDuff.Mode.MULTIPLY)
                 val drawable = dataSourceSmallImage!!.loadDrawable(context)
                 drawable!!.colorFilter = mColorFilter
                 val newIcon = Icon.createWithBitmap(drawable.toBitmap())
                 dataSourceSmallImage = newIcon
-                //dataSourceSmallImage!!.setTintMode(PorterDuff.Mode.OVERLAY)
-                //dataSourceSmallImage!!.setTint(Color.RED)
+                dataSourceSmallImage!!.setTintMode(PorterDuff.Mode.MULTIPLY)
+                dataSourceSmallImage!!.setTint(Color.RED)
             }
         }
         if (complicationWireData!!.hasBurnInProtectionSmallImage()) {
