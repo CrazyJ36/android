d405f085af6be45bd9f92f2fdb22f4a13cfaaac5
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-21 23:10:33 -0500

    u

--- android-studio-projects/ComplicationTest/app/build.gradle.kts
+++ android-studio-projects/ComplicationTest/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.crazyj36.complicationtest"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
