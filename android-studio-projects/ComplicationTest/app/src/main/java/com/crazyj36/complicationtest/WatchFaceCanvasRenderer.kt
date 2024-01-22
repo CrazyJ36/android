2a82dc1ec8d5845ddb1d9bf6062b412794d627dc
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-21 23:08:29 -0500

    u

--- android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
+++ android-studio-projects/ComplicationTest/app/src/main/java/com/crazyj36/complicationtest/WatchFaceCanvasRenderer.kt
@@ -1,11 +1,6 @@
 package com.crazyj36.complicationtest
 
-import android.annotation.SuppressLint
-import android.app.PendingIntent
-import android.content.ComponentName
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.BlendMode
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.ColorMatrixColorFilter
@@ -13,7 +8,6 @@ import android.graphics.Paint
 import android.graphics.PorterDuff
 import android.graphics.Rect
 import android.graphics.drawable.Icon
-import android.support.wearable.complications.ComplicationData
 import android.util.Log
 import android.view.SurfaceHolder
 import androidx.core.content.res.ResourcesCompat
@@ -24,11 +18,8 @@ import androidx.wear.watchface.Renderer
 import androidx.wear.watchface.WatchState
 import androidx.wear.watchface.complications.data.ComplicationType
 import androidx.wear.watchface.complications.data.MonochromaticImage
-import androidx.wear.watchface.complications.data.PlainComplicationText
 import androidx.wear.watchface.complications.data.ShortTextComplicationData
-import androidx.wear.watchface.complications.data.SmallImageComplicationData
 import androidx.wear.watchface.style.CurrentUserStyleRepository
-import java.time.LocalDateTime
 import java.time.ZonedDateTime
 
 class WatchFaceCanvasRenderer(
@@ -46,21 +37,10 @@ class WatchFaceCanvasRenderer(
     interactiveDrawModeUpdateDelayMillis = 1000L,
     clearWithBackgroundTintBeforeRenderingHighlightLayer = false
 ) {
-
     private val tag = "COMPLICATION_TEST"
     private var complication: ComplicationSlot? = null
-    private var complicationWireData: ComplicationData? = null
     private var shortTextComplicationDataBuilder: ShortTextComplicationData.Builder? = null
     private var shortTextComplicationData: ShortTextComplicationData? = null
-    private var smallImageComplicationDataBuilder: SmallImageComplicationData.Builder? = null
-    private var smallImageComplicationData: SmallImageComplicationData? = null
-    private var dataSourceDataSource: ComponentName? = null
-    private var dataSourceTapAction: PendingIntent? = null
-    private var dataSourceText: CharSequence? = null
-    private var dataSourceContentDescription: CharSequence? = null
-    private var dataSourceTitle: CharSequence? = null
-    private var dataSourceIcon: Icon? = null
-    private var dataSourceBurnInProtectionSmallImage: Icon? = null
     private val paint = Paint()
     private var colorMatrix = floatArrayOf(
         0.4f, 0.4f, 0.4f, 0f, 0f,
@@ -77,47 +57,16 @@ class WatchFaceCanvasRenderer(
     ) {
     }
 
-    @SuppressLint("RestrictedApi", "DiscouragedApi")
     override fun render(
         canvas: Canvas,
         bounds: Rect,
         zonedDateTime: ZonedDateTime,
         sharedAssets: MySharedAssets
     ) {
-        complication = null
-        complicationWireData = null
         complication = complicationSlotsManager.complicationSlots[0]
-        complicationWireData = complication!!.complicationData.value.asWireComplicationData()
-        shortTextComplicationDataBuilder = null
-        shortTextComplicationData = null
-        smallImageComplicationDataBuilder = null
-        smallImageComplicationData = null
-        dataSourceDataSource = null
-        dataSourceTapAction = null
-        dataSourceText = null
-        dataSourceContentDescription = null
-        dataSourceTitle = null
-        dataSourceIcon = null
-        dataSourceBurnInProtectionSmallImage = null
-
         val data = complication!!.complicationData.value.toString()
-        Log.d(tag, data)
-
-        //getDataSourceInfo(zonedDateTime)
         when (complication!!.complicationData.value.type) {
             ComplicationType.SHORT_TEXT -> {
-                /*ShortTextComplicationData(text=ComplicationText{
-                mSurroundingText=REDACTED, mTimeDependentText=null, mDynamicText=null},
-                title=null, monochromaticImage=MonochromaticImage(
-                image=Icon(typ=RESOURCE pkg=com.google.android.wearable.sysui id=0x7f08025a),
-                ambientImage=Icon(typ=RESOURCE pkg=com.google.android.wearable.sysui id=0x7f08011a)),
-                smallImage=null, contentDescription=ComplicationText{mSurroundingText=REDACTED,
-                mTimeDependentText=null, mDynamicText=null}, tapActionLostDueToSerialization=false,
-                tapAction=PendingIntent{af660ce: android.os.BinderProxy@e02b051}, validTimeRange=TimeRange(REDACTED),
-                dataSource=ComponentInfo{com.google.android.wearable.sysui/
-                com.google.android.clockwork.sysui.experiences.complications.providers.BatteryProviderService},
-                persistencePolicy=0, displayPolicy=0, dynamicValueInvalidationFallback=null)
-                 */
                 val imagePkg = data.split("pkg=")[1].split(" id=")[0]
                 val imageId = data.split("id=")[1].split(")")[0]
                 shortTextComplicationData = complication!!.complicationData.value as ShortTextComplicationData
@@ -127,7 +76,7 @@ class WatchFaceCanvasRenderer(
                 ).setMonochromaticImage(MonochromaticImage.Builder(
                     Icon.createWithResource(imagePkg, Integer.decode(imageId))
                 ).build()
-                )
+                ).setTapAction(shortTextComplicationData!!.tapAction)
                 complication!!.renderer.loadData(shortTextComplicationDataBuilder!!.build(), false)
                 complication!!.render(canvas, zonedDateTime, renderParameters)
             }
@@ -176,97 +125,6 @@ class WatchFaceCanvasRenderer(
             )
         }
     }
-
-    /*@SuppressLint("RestrictedApi")
-    private fun setShortTextComplicationData(): ShortTextComplicationData {
-
-
-
-        if (dataSourceText != null && dataSourceContentDescription != null) {
-            Log.d(tag, "Setting dataSourceText and dataSourceContentDescription.")
-            shortTextComplicationDataBuilder = ShortTextComplicationData.Builder(
-                PlainComplicationText.Builder(dataSourceText!!).build(),
-                PlainComplicationText.Builder(dataSourceContentDescription!!).build()
-            )
-        } else if (dataSourceText != null) {
-            Log.d(tag, "Setting dataSourceText, no dataSourceContentDescription.")
-            shortTextComplicationDataBuilder = ShortTextComplicationData.Builder(
-                PlainComplicationText.Builder(dataSourceText!!).build(),
-                PlainComplicationText.Builder("Content description not supplied by DataSource.")
-                    .build()
-            )
-        }
-        if (shortTextComplicationDataBuilder != null) {
-            if (dataSourceDataSource != null) {
-                Log.d(tag, "Setting dataSourceDataSource.")
-                shortTextComplicationDataBuilder!!.setDataSource(dataSourceDataSource)
-            }
-            if (dataSourceTapAction != null) {
-                Log.d(tag, "Setting dataSourceTapAction.")
-                shortTextComplicationDataBuilder!!.setTapAction(dataSourceTapAction)
-            }
-            if (dataSourceTitle != null) {
-                Log.d(tag, "Setting dataSourceTitle.")
-                shortTextComplicationDataBuilder!!.setTitle(
-                    PlainComplicationText.Builder(dataSourceTitle!!).build()
-                )
-            }
-            if (dataSourceIcon != null) {
-                Log.d(tag, "Setting dataSourceIcon.")
-                shortTextComplicationDataBuilder!!.setMonochromaticImage(
-                    MonochromaticImage.Builder(dataSourceIcon!!).build()
-                )
-            }
-            shortTextComplicationData = shortTextComplicationDataBuilder!!.build()
-        }
-        return if (shortTextComplicationData != null) {
-            Log.d(tag, "Custom shortTextComplicationData creation complete.")
-            shortTextComplicationData!!
-        } else {
-            Log.d(tag, "dataSourceText was null, loading placeholder...")
-            ShortTextComplicationData.Builder(
-                PlainComplicationText.Builder("No data").build(),
-                PlainComplicationText.Builder("No data.").build()
-            ).build()
-        }
-    }
-
-    @SuppressLint("RestrictedApi") // applying attributes here works.
-    private fun getDataSourceInfo(zonedDateTime: ZonedDateTime) {
-        if (complicationWireData!!.dataSource != null) {
-            Log.d(tag, "hasDataSource")
-            dataSourceDataSource = complication!!.complicationData.value.dataSource
-        }
-        if (complicationWireData!!.hasTapAction()) {
-            Log.d(tag, "hasTapAction")
-            dataSourceTapAction = complication!!.complicationData.value.tapAction
-        }
-        if (complicationWireData!!.hasShortText()) {
-            Log.d(tag, "hasShortText")
-            dataSourceText = complicationWireData!!.shortText!!.getTextAt(
-                Resources.getSystem(),
-                LocalDateTime.now().atZone(zonedDateTime.zone).toInstant().toEpochMilli()
-            )
-        }
-        if (complicationWireData!!.hasContentDescription()) {
-            Log.d(tag, "hasContentDescription")
-            dataSourceContentDescription = complicationWireData!!.contentDescription!!.getTextAt(
-                Resources.getSystem(),
-                LocalDateTime.now().atZone(zonedDateTime.zone).toInstant().toEpochMilli()
-            )
-        }
-        if (complicationWireData!!.hasShortTitle()) {
-            Log.d(tag, "hasShortTitle")
-            dataSourceTitle = complicationWireData!!.shortTitle!!.getTextAt(
-                Resources.getSystem(),
-                LocalDateTime.now().atZone(zonedDateTime.zone).toInstant().toEpochMilli()
-            )
-        }
-        if (complicationWireData!!.hasIcon()) {
-            Log.d(tag, "hasIcon")
-            dataSourceIcon = complicationWireData!!.icon!!
-        }
-    }*/
 
     class MySharedAssets : SharedAssets {
         override fun onDestroy() {
