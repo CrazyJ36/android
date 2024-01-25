1c40dd8489bbae4ba1de0aae76ab7e6ae34676e0
CrazyJ36 <crazy_j36@hotmail.com>
2024-01-24 23:57:11 -0500

    uu

--- android-studio-projects/CircularImageView/app/build.gradle.kts
+++ android-studio-projects/CircularImageView/app/build.gradle.kts
@@ -6,20 +6,13 @@ plugins {
 android {
     namespace = "com.crazyj36.circularimageview"
     compileSdk = 34
-
     defaultConfig {
         applicationId = "com.crazyj36.circularimageview"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -32,14 +25,8 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("com.google.android.material:material:1.11.0")
 }
--- android-studio-projects/CircularImageView/app/src/main/java/com/crazyj36/circularimageview/MainActivity.kt
+++ android-studio-projects/CircularImageView/app/src/main/java/com/crazyj36/circularimageview/MainActivity.kt
@@ -1,94 +1,11 @@
 package com.crazyj36.circularimageview
 
 import android.os.Bundle
-import android.util.Log
-import android.util.TypedValue
-import android.widget.RelativeLayout
 import androidx.activity.ComponentActivity
-import androidx.core.view.setMargins
-import com.google.android.material.imageview.ShapeableImageView
-import com.google.android.material.shape.ShapeAppearanceModel
-import java.util.Timer
-import java.util.TimerTask
 
 class MainActivity : ComponentActivity() {
-    private val tag = "CIRCULAR_IMAGEVIEW"
-    private val timer = Timer()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val shapeableImageView = findViewById<ShapeableImageView>(R.id.shapeableImageView)
-
-        shapeableImageView.setStrokeColorResource(
-            android.R.color.holo_blue_dark
-        )
-        shapeableImageView.shapeAppearanceModel =
-            ShapeAppearanceModel.builder(
-                applicationContext,
-                R.style.CircularShapeModel,
-                R.style.CircularShapeModel
-            ).build()
-
-        var strokeWidthFloat = 2f
-        var shapeableImageViewMargin = 2f
-        var upOrDown = true
-        var count = 0
-        class MyTimerTask : TimerTask() {
-            override fun run() {
-                if (upOrDown && count < 20) {
-                    strokeWidthFloat += 0.3f
-                    shapeableImageViewMargin += 0.3f
-                    count++
-                    if (count == 20) upOrDown = false
-                } else if (!upOrDown && count > 0) {
-                    strokeWidthFloat -= 0.3f
-                    shapeableImageViewMargin -= 0.3f
-                    count--
-                    if (count == 0) upOrDown = true
-                }
-                Log.d(tag, "count: $count")
-
-                shapeableImageView.strokeWidth = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP, strokeWidthFloat, resources.displayMetrics
-                )
-                TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP, shapeableImageViewMargin, resources.displayMetrics
-                ).toInt()
-
-                val layoutParamsSize = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP, 128f, resources.displayMetrics
-                ).toInt()
-                val shapeableImageViewLayoutParams = RelativeLayout.LayoutParams(
-                    layoutParamsSize,
-                    layoutParamsSize
-                )
-                shapeableImageViewLayoutParams
-                    .addRule(RelativeLayout.CENTER_IN_PARENT,
-                        RelativeLayout.TRUE
-                    )
-
-                val pixels = TypedValue.applyDimension(
-                    TypedValue.COMPLEX_UNIT_DIP,
-                    shapeableImageViewMargin,
-                    resources.displayMetrics
-                )
-                runOnUiThread() {
-                    shapeableImageViewLayoutParams.setMargins(
-                        pixels.toInt()
-                    )
-                    shapeableImageView.layoutParams = shapeableImageViewLayoutParams
-                }
-
-                shapeableImageView.invalidate()
-            }
-
-        }
-        timer.schedule(MyTimerTask(), 0, 16)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        timer.cancel()
-        timer.purge()
     }
 }
